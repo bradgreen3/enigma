95c3e0ee885a01bec0b8120d5b601ea6e0c732a3
Lee Saville <l.w.saville@gmail.com>
2016-08-27 16:38:11 -0600

    Delete most of what I did to make it simpler. Complete key_generator with testing

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,20 +1,33 @@
 class KeyGenerator
-  attr_reader :numbers, :key, :shuffled
+  attr_reader :numbers, :key
   def initialize
     @numbers = [0,1,2,3,4,5,6,7,8,9]
     @key = []
   end
 
-  def shuffle
-    @shuffled = @numbers.shuffle
+  def get_key
+    5.times { @key << @numbers.sample }
   end
 
-  def key_array
-    self.shuffled.each_with_index do |num, index|
-      if index % 2 != 0
-        self.key << num
-      end
+  def give_key
+    if @key.empty?
+      self.get_key
+    else
+      @key.join
     end
   end
 
 end
+
+
+  # def shuffle
+  #   @shuffled = @numbers.shuffle
+  # end
+  #
+  # def key_array
+  #   self.shuffled.each_with_index do |num, index|
+  #     if index % 2 != 0
+  #       self.key << num
+  #     end
+  #   end
+  # end
